6659b792605839feb5804161a326bc29624ab8ad
Brant <brant@gurganus.name>
2022-09-26 13:05:27 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { foods } from "./food";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold">Hello world!</h1>
+      <div className="flex flex-wrap">
+        {foods.map((food) => {
+          return (
+            <div
+              className="p-2 shadow-lg hover:bg-indigo-600 hover:text-white border flex max-w-sm flex-row m-2 rounded"
+              key={food.name}
+            >
+              <h2 className="m-2 text-3xl">{food.name}</h2>
+              <img
+                className="h-52"
+                src={"/images/" + food.image}
+                alt={food.name}
+              />
+              <p>{food.description}</p>
+              <p>{food.price}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
