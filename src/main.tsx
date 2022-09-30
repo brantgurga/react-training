28ae7d0bf239e04ffe6b4f8593491bd6888272bd
Brant <brant@gurganus.name>
2022-09-30 10:14:46 -0400

    sync up changes

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,18 @@ import { Toaster } from "react-hot-toast";
 import "./index.css";
 import { ErrorBoundary } from "react-error-boundary";
 
+// Lazy load so this isn't in the prod bundle
+const DevTools = React.lazy(() => import("./mocks/DevTools"));
+
+const useDevTools = import.meta.env.VITE_ENABLE_DEVTOOLS === "Y";
+
 // Warning: StrictMode will render twice in dev only. This can catch subtle bugs.
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<h1>Oops! Sorry, an error occurred.</h1>}>
       <BrowserRouter>
         <Toaster />
-        <App />
+        {useDevTools ? <DevTools /> : <App />}
       </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
